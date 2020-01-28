a5068d53635546a4693942786d6802cb4147bbc4
Yannick Lang <layaxx@gmx.de>
2020-01-29 00:19:15 +0100

    ColourPool added and displayed

--- mastermind.js
+++ mastermind.js
@@ -7,26 +7,65 @@ class Colour {
 
 }
 
-class Combination{
+class Combination {
 
-    constructor(){
-        this.colours = new Colour[combinationLength];
-        var randomColour;
-        for(var i = 0; i < combinationLength; i++){
-            randomColour =
-            this.colours[i] = randomColour;
+    constructor(colours) {
+        if (colours != null) {
+            console.log(typeof colours);
+            this.colours = colours;
+        } else {
+            this.colours = [];
+            var randomColour;
+            for (var i = 0; i < combinationLength; i++) {
+                do {
+                    randomColour = colourPool[Math.floor(Math.random() * coloursAmount)];
+                }
+                while (this.colours.indexOf(randomColour) != -1)
+                this.colours[i] = randomColour;
+            }
         }
     }
-
-    constructor(colours){
-        this.colours = colours;
-    }
 }
+
+
 
 
 var combinationLength = 5       // coloursAmount must be greater than (or euqal to) combinationsLength !!
 var coloursAmount = 8;
 
-var colors = new Colour[8];
-var guesses = new Colour[1][combinationLength];
-var combination = new Combination();
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+
+var colourPool = [];
+colourPool[0] = new Colour('white', 'FFFFFF');
+colourPool[1] = new Colour('black', '000000');
+colourPool[2] = new Colour('red', 'BB0000');
+colourPool[3] = new Colour('yellow', 'FFFF00');
+colourPool[4] = new Colour('blue', '0000BB');
+colourPool[5] = new Colour('green', '00BB00');
+colourPool[6] = new Colour('orange', 'ffa500');
+colourPool[7] = new Colour('shit', '555555');
+var guesses = [];
+var combination = new Combination(undefined);
+
+
+for (x of combination.colours) {
+    console.log(x.name);
+}
+
+
+
+function displayColourPool() {
+    var spacePerColour = canvas.width / coloursAmount;
+
+
+    for (i = 0; i < coloursAmount; i++) {
+        ctx.fillStyle = "#" + colourPool[i].value;
+        ctx.fillRect(i * spacePerColour, canvas.height - 100,spacePerColour, 100);
+    }
+    ctx.fillStyle = "#000000";
+    ctx.fillRect(0, canvas.height - 105, canvas.width, 5);
+}
+
+displayColourPool();
